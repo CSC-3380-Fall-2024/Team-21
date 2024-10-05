f227fff2d3ca368bfee7850d48789deeca36c9cf
Athor31 <Athor31@lsu.edu>
2024-10-05 16:15:21 -0500

    User Profile Update

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Tiger_Tasks.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+       
+        public string Major { get; set; }
+
+    }
+}

b72c5e2d66a35cb18b5f3f2ad33735964cda2796
Athor31 <Athor31@lsu.edu>
2024-11-01 21:37:46 -0500

    Backend for te forum post creation

--- Controllers/ForumController.cs
+++ Controllers/ForumController.cs
@@ -39,7 +39,7 @@ namespace Tiger_Tasks.Controllers
                 return RedirectToAction(nameof(Index));
 
             }
-            return View();
+            return View(post);
         }
     }
 }
